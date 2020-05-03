18682c16752765e285e5cc38ebd64474df96a113
Tobipeters <oluwatobilobapeterschem@gmail.com>
2020-05-03 18:11:04 +0100

    Ui design done, development now on

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 30DaysOfCode_Day_3
 BMI (Body Mass Index) Calculator 
+Hosting link below 
+https://tobipeters.github.io/30DaysOfCode_Day_3/
---
+++ script.js
@@ -0,0 +1,14 @@
+var bmiResult = document.getElementById('result');
+var title = document.getElementById('title');
+
+//function for calculating BMI value 
+calculateBmi = () =>{
+    let heightValue = document.getElementById('height_value').value;
+    let weightValue = document.getElementById('weight_value').value;
+    let heightUnit = document.getElementById('height_unit').value;
+    let weightUnit = document.getElementById('height_unit').value;
+
+    if (weightUnit === '' && height) {
+        
+    }
+}
